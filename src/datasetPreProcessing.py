78dbb1b8f420e614d1bb445640d0a568afa91700
Xiangdong Xie <xxd@Xiangdongs-MacBook-Pro.local>
2023-06-10 04:07:43 -0400

    removing some test code and archiving codes no longer active

---
+++ src/datasetPreProcessing.py
@@ -0,0 +1,71 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+from sklearn.compose import make_column_transformer, make_column_selector
+
+# data loader functions and data preprocessing functions
+def LoadDatasetByName(DATASET, *args):
+    raw_data, anomalous_raw, features, categorical_data_index = None, None, None, None
+    if DATASET == 'original_data':
+        raw_data = np.loadtxt('../data/original_regular_data.csv', skiprows=1, delimiter=',')[:,:-1]
+        anomalous_raw = np.loadtxt('../data/original_attack_data.csv', skiprows=1, delimiter=',')[:,:-1]
+        categorical_data_index = [0,1]
+    if DATASET == 'network_data':
+        raw_data = np.loadtxt('../data/network_flow_regular_data.csv', skiprows=1, delimiter=',')[:,:-1]
+        anomalous_raw = np.loadtxt('../data/network_flow_attack_data.csv', skiprows=1, delimiter=',')[:,:-1]
+        """ idc = np.bool_(np.loadtxt('../data/idc/data_idc1.csv', delimiter=','))
+        anomalous_raw = anomalous_raw[idc,:]
+        idc = np.bool_(np.loadtxt('../data/idc/data_idc2.csv', delimiter=','))
+        anomalous_raw = anomalous_raw[idc,:] """
+        #anomalous_raw = np.loadtxt('../data/network_flow_attack_data_undetected.csv', delimiter=',')[:,:-1]
+    elif DATASET == 'medical_data':
+        raw_data = np.loadtxt('../data/medical_regular_data.csv', skiprows=1, delimiter=',')[:,:-1]
+        anomalous_raw = np.loadtxt('../data/medical_attack_data.csv', skiprows=1, delimiter=',')[:,:-1]
+        idc = np.bool_(np.loadtxt('../data/idc/data_idc1.csv', delimiter=','))
+        anomalous_raw = anomalous_raw[idc,:]
+        #anomalous_raw = np.loadtxt('../data/medical_attack_data_undetected.csv', skiprows=1, delimiter=',')[:,:-1]
+    elif DATASET == 'full_data':
+        raw_data = np.concatenate((np.loadtxt('../data/network_flow_regular_data.csv', skiprows=1, delimiter=',')[:,:-1], np.loadtxt('../data/medical_regular_data.csv', skiprows=1, delimiter=',')[:,:-1]), axis=1)
+        anomalous_raw = np.concatenate((np.loadtxt('../data/network_flow_attack_data.csv', skiprows=1, delimiter=',')[:,:-1], np.loadtxt('../data/medical_attack_data.csv', skiprows=1, delimiter=',')[:,:-1]), axis=1)
+        features = np.concatenate((np.loadtxt('../data/network_flow_regular_data.csv', max_rows=1, delimiter=',', dtype=str), np.loadtxt('../data/medical_regular_data.csv', max_rows=1, delimiter=',', dtype=str)), axis=0)
+        """ idc = np.bool_(np.loadtxt('../data/idc/data_idc1.csv', delimiter=','))
+        anomalous_raw = anomalous_raw[idc,:]
+        idc = np.bool_(np.loadtxt('../data/idc/data_idc2.csv', delimiter=','))
+        anomalous_raw = anomalous_raw[idc,:] """
+    elif DATASET == 'network_data1':
+        raw_data = np.loadtxt('../data/network_flow1_regular_data.csv', skiprows=1, delimiter=',')[:,:-1]
+        anomalous_raw = np.loadtxt('../data/network_flow1_attack_data.csv', skiprows=1, delimiter=',')[:,:-1]
+    elif DATASET == 'medical_data1':
+        #raw_data = np.loadtxt('../data/medical_regular_data.csv', skiprows=1, delimiter=',')[:,:-1]
+        raw_data = np.loadtxt('../data/medical1_regular_data.csv', skiprows=1, delimiter=',')[:,:-1]
+        #raw_data = np.concatenate((np.loadtxt('../data/medical_regular_data.csv', skiprows=1, delimiter=',')[:,:-1], np.loadtxt('../data/medical1_regular_data.csv', skiprows=1, delimiter=',')[:,:-1]), axis=0)
+        anomalous_raw = np.loadtxt('../data/medical_attack_data.csv', skiprows=1, delimiter=',')[:,:-1]
+        #anomalous_raw = np.loadtxt('../data/medical_attack_data_undetected.csv', skiprows=1, delimiter=',')[:,:-1]
+    elif DATASET == 'full_data1':
+        raw_data = np.concatenate((np.loadtxt('../data/network_flow1_regular_data.csv', skiprows=1, delimiter=',')[:,:-1], np.loadtxt('../data/medical1_regular_data.csv', skiprows=1, delimiter=',')[:,:-1]), axis=1)
+        anomalous_raw = np.concatenate((np.loadtxt('../data/network_flow1_attack_data.csv', skiprows=1, delimiter=',')[:,:-1], np.loadtxt('../data/medical1_attack_data.csv', skiprows=1, delimiter=',')[:,:-1]), axis=1)
+    elif DATASET == 'network_data2':
+        raw_data = np.loadtxt('../data/network_flow2_regular_data.csv', skiprows=1, delimiter=',')[:,:-1]
+        anomalous_raw = np.loadtxt('../data/network_flow2_attack_data.csv', skiprows=1, delimiter=',')[:,:-1]
+    elif DATASET == 'iiot_data':
+        raw_data = np.loadtxt('../data/iiot_regular_data.csv', skiprows=1, delimiter=',')[:,:-1]
+        anomalous_raw = np.loadtxt('../data/iiot_attack_data.csv', skiprows=1, delimiter=',')[:,:-1]
+        features = np.loadtxt('../data/iiot_attack_data.csv', max_rows=1, delimiter=',', dtype=str)
+    elif DATASET == 'ECU_IoHT_data':
+        raw_data = np.loadtxt('../data_backup/ECU_IoHT_regular_data.csv', skiprows=1, delimiter=',', dtype=str)[:,:-2]
+        anomalous_raw = np.loadtxt('../data_backup/ECU_IoHT_attack_data.csv', skiprows=1, delimiter=',', dtype=str)[:,:-2]
+        features = np.loadtxt('../data_backup/ECU_IoHT_attack_data.csv', max_rows=1, delimiter=',', dtype=str)
+        categorical_data_index = [1,2,3]
+    # return loaded data
+    return raw_data, anomalous_raw, features, categorical_data_index
+
+def OneHotTransformer(X, index):
+    ohe = OneHotEncoder(categories='auto', handle_unknown='infrequent_if_exist', dtype=np.float64, sparse_output=False)
+    column_transformer = make_column_transformer(
+        (ohe, index),
+        remainder='passthrough'
+    )
+    column_transformer.fit(X)
+    return column_transformer
+
+
